bd41d9574265e4b5063236c07dc97144c0ae6875
Ricardo Sousa <ricardofcs84@gmail.com>
2023-01-27 16:37:18 +0000

    Test commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
-      <h2>Produtos mais vendidos</h2>
+      <h2>Produtos mais vendidos!</h2>
       {/* <p>speaker There are many variations passages</p> */}
     </div>
 
